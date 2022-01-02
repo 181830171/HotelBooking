39117adad84e3dce4ecc4777df621b577c98bd16
xhw <1677676052@qq.com>
2022-01-02 16:52:01 +0800

    feat: hotelList

--- front_end/src/views/hotel/components/hotelCard.vue
+++ front_end/src/views/hotel/components/hotelCard.vue
@@ -1,24 +1,28 @@
 <template>
-    <div class="hotelCard" style="margin:10px ;width: 48%;height:250px;display:flex;border: 1px lightgray solid">
+    <div class="hotelCard" style="margin:10px ;width: 45%;height:300px;display:flex;border: 1px lightgray solid">
+      <div class = "imgBox">
         <img
-          alt="example"
-          :src="hotel.picture"
+            alt="example"
+            :src="hotel.picture"
         />
-      <a-descriptions :title="hotel.name" size="default" style="margin: 10px" :column="1">
-          <a-descriptions-item label="星级" >
-              <a-rate style="font-size: 15px" :value="list.indexOf(hotel.hotelStar)+1" disabled allowHalf/>
+      </div>
+      <div class="descriptions">
+        <a-descriptions :title="hotel.name" size="default" style="margin: 10px;" :column="1">
+          <a-descriptions-item label="星级">
+            <a-rate style="font-size: 15px" :value="list.indexOf(hotel.hotelStar)+1" disabled allowHalf/>
           </a-descriptions-item>
           <a-descriptions-item label="评分">
-              <p v-if="hotel.rate!=0">{{hotel.rate}}分</p>
-              <p v-else>该酒店暂无评分</p>
+            <p v-if="hotel.rate!=0">{{hotel.rate}}分</p>
+            <p v-else>该酒店暂无评分</p>
           </a-descriptions-item>
           <a-descriptions-item label="地址">
-              {{hotel.address}}
+            {{hotel.address}}
           </a-descriptions-item>
-        <a-descriptions-item label="最低">
-          <span style="font-size:1.5em;font-weight: bold;color: #ff6666">￥{{lowestPrice}}</span>/起
-        </a-descriptions-item>
-      </a-descriptions>
+          <a-descriptions-item label="最低">
+            <span style="font-size:1.5em;font-weight: bold;color: #ff6666">￥{{lowestPrice}}</span>/起
+          </a-descriptions-item>
+        </a-descriptions>
+      </div>
     </div>
 </template>
 <script>
@@ -52,12 +56,56 @@ export default {
 }
 </script>
 <style scoped lang="less">
-
-    img {
-      height: 250px;
-        width:200px
+    .hotelCard {
+      position: relative;
+      border-radius: 5px;
+    }
+    .imgBox {
+      width: 100%;
+      overflow: hidden;
+      border-radius: 5px;
+    }
+    .imgBox img {
+      width: 100%;
     }
 
+    .hotelCard .imgBox img{
+      transition: transform 1s;
+    }
+    .hotelCard:hover .imgBox img{
+      transform: scale(1.2);
+    }
+    // 阴影盒子
+    .descriptions {
+      position: absolute;
+      top:10px;
+      right:10px;
+      left:10px;
+      bottom:10px;
+      background: rgba(0,0,0,0.6);
+      transform: scale(0);
+      transition: transform .7s;
+      border-radius: 5px;
+    }
+    .hotelCard:hover .descriptions {
+      transform: scale(1);
+    }
+    .descriptions ::v-deep .ant-descriptions-title {
+      margin-bottom: 20px;
+      color:rgba(255,255,255,0.85);
+      font-weight: normal;
+      font-size: 22px;
+      line-height: 1.5;
+    }
+    .descriptions ::v-deep .ant-descriptions-item-label{
+      color:#fff;
+      font-size: 11px;
+    }
+    .descriptions ::v-deep .ant-descriptions-item-content{
+      color:#fff;
+      font-size: 11px;
+    }
+    
 </style>
 
 <style lang="less">
